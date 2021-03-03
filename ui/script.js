cb2c72e50cd576a237a237e04efe1d8a3c5ff87e
Swqpping <swqpping@gmail.com>
2021-03-03 05:08:44 +0100

    added ui folder

---
+++ ui/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  window.addEventListener('message', function (event) {
+    var item = event.data;
+    if (item.show == true) {
+      openHome();
+    }
+    if (item.show == false) {
+      close();
+    }
+
+  });
+  document.onkeyup = function (data) {
+    if (data.which == 27) {
+      $.post('http://bugreports/close', JSON.stringify({}));
+    }
+  };
+
+
+  $(".sendReport").click(function () {
+    var discord = document.getElementById("discord").value;
+    var description = document.getElementById("description").value;
+    if (discord == "" || description == "") {
+      console.log("Something went wrong")
+    }
+    else {
+      data = [discord,description];
+      $.post('http://bugreports/sendReport', JSON.stringify({data}));
+    }
+  });
+
+});
+
+
+function openHome() {
+  $("body").css("display", "block");
+}
+
+function close() {
+  $("body").css("display", "none");
+}
