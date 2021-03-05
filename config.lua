7ba3c30b4b1c71ae0f22ce88b8abe20dbc7f7085
Swqpping <58861408+Swqppingg@users.noreply.github.com>
2021-03-05 04:04:56 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 
 -- Change this to your discord webhook link for notifications when a new bug report is sent
-Config.discordwebhooklink = 'https://discord.com/api/webhooks/740430639584772138/sdNP8oiVVGtOoU2MUAfbQrvjohlIPUi34K6W6dXA59M9d-JF93pNOsd0YogeYwP1j02t'
+Config.discordwebhooklink = ''
 
 
 
